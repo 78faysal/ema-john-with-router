6c77ccff7505fda42e281ffbc7e9ce6f8e747066
mdfaysal <faysal7830@gmail.com>
2021-12-13 18:26:55 -0800

    updated text

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is Inventory</h2>
+            <h2>This is Inventory. And welcome here you guys!!</h2>
         </div>
     );
 };
